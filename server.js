821d3253c57019075b466fd829c9c43a57b86884
NathanCarr10 <[nathancarr18@gmail.com]>
2024-10-25 13:48:26 +0000

    exercise 4 complete

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to Data Representation & Querying');
 });
 
+//modifying URL to display my first name and last name
+app.get('/hello/:nathan/:carr', (req, res)=>{
+    res.send("Hello "+req.params.name+" "+req.params.lname);
+})
+
 //listen to port:3000
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
