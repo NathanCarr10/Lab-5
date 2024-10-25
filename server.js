e2c313b53ce19c770f832f02e9e2902f721cd6dc
NathanCarr10 <[nathancarr18@gmail.com]>
2024-10-25 13:22:18 +0000

    modified server.js file, added code and "Welcome to Data Rep" Message

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Welcome to Data Respresentation & Querying');
 });
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
