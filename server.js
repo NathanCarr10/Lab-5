eb694c21e2716131b7fb007877a291bb49dbaf22
NathanCarr10 <[nathancarr18@gmail.com]>
2024-10-25 13:35:23 +0000

    exercise 3 finished

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+//including express extension
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Data Respresentation & Querying');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-
+//adding error-handling middleware to catch any server errors
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
+});
+
+//Displaying Welcome message to be called from server via get method
+app.get('/', (req, res) => {
+    res.send('Welcome to Data Representation & Querying');
+});
+
+//listen to port:3000
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
